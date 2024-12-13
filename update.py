ca403017a123792d2b738fa7fc50a83a1f4f5498
abdullahsakib <abdullahsakib33@gmail.com>
2024-12-13 20:22:40 +0600

    bh

--- update.py
+++ update.py
@@ -1,12 +1,13 @@
 from datetime import datetime
 from save import save_book
+
 def update_books(all_books):
     search_book = input("Enter Book Title to Update: ")
     for book in all_books:
         if book["title"] == search_book:
-            title = input("Enter Book Title: ")
-            author = input("Enter Author Name: ")
-            year = int(input("Enter Publishing Year Number: "))
+            title = input("Enter Book Title: ") or book["title"]
+            author = input("Enter Author Name: ") or book['author']
+            year = int(input("Enter Publishing Year Number: ")) or book['year']
             price = int(input("Enter Book Price: "))
             quantity = int(input("Enter Quantity Number: "))
 
